43bb397c2a934009f42441f9ad4f3220d6f869db
Alex Aalbertsberg <alexaalbertsberg@gmail.com>
2017-01-23 18:32:05 +0000

    Performances backend

--- app/Http/Controllers/BackendControllers/PerformanceController.php
+++ app/Http/Controllers/BackendControllers/PerformanceController.php
@@ -38,14 +38,28 @@ class PerformanceController extends Controller
     public function edit($id){
         $performance = Performance::findOrFail($id);
 
-        return view('amber.backend.performances.edit', compact($performances, 'performances'));
+        return view('amber.backend.performances.edit', compact($performance, 'performance'));
     }
 
     public function update($id, Request $request){
         // perform update
         $performance = Performance::findOrFail($id);
 
-        $performance = Performance::update($request->all());
+        $performance = $performance->update($request->all());
+
+        $image = $request->file('mediaItem');
+        // if new image has been uploaded, save it, otherwise ignore.
+        if($image != null)
+        {
+            // after update, Laravel seems to lose the reference to $performance, so we just fetch it again for now.
+            $performance = Performance::findOrFail($id);
+
+            if($image->move(public_path("/uploads"), $image->getClientOriginalname())){
+                $performance->mediaItem = $image->getClientOriginalname();
+            }
+
+            $performance->save();
+        }
 
         return redirect()->route('performances.index');
     }
--- resources/views/amber/backend/performances/index.blade.php
+++ resources/views/amber/backend/performances/index.blade.php
@@ -29,9 +29,21 @@
 
   @foreach ($performances as $performance)
       <h1>{{$performance->name}}</h1>
+      <h1>{{$performance->description}}</h1>
       <h2>{{$performance->performanceDate}}</h2>
       <img src="/uploads/{{$performance->mediaItem}}" alt="{{$performance->name}}">
       <h2>{{date('F d, Y', strtotime($performance->created_at))}}</h2>
+      <a href="{{Request::url()}}/{{$performance->id}}/edit" class="btn btn-default btn-lg">
+          <span class="glyphicon glyphicon-pencil"></span> Edit
+      </a>
+      <form method="POST" action="{{URL::to('/Amber/performances')}}/{{$performance->id}}" enctype="multipart/form-data">
+          <input type="hidden" name="_method" value="DELETE">
+          <input type="hidden" name="_token" value="{{csrf_token()}}">
+          <button type="submit" class="btn btn-default btn-lg">
+              <span class="glyphicon glyphicon-remove"></span> Delete
+          </button>
+      </form>
+      <hr>
   @endforeach
 
 @endsection
