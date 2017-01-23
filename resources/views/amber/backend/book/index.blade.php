294818817ed32b93d5691ba4cf30f558fcd5b1fd
Alex Aalbertsberg <alexaalbertsberg@gmail.com>
2017-01-23 19:39:59 +0000

    BOOK LUL

--- resources/views/amber/backend/book/index.blade.php
+++ resources/views/amber/backend/book/index.blade.php
@@ -11,21 +11,22 @@
 </head>
 <body>
 
+<h1>Behold, the Book of the Dankest Twitch Chat Memes</h1>
 <div class="flipbook-viewport">
 	<div class="container">
 		<div class="flipbook">
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
-			<div style="background-image:url(https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg)"></div>
+			<div style="background-image:url(https://pbs.twimg.com/media/CQyQbkiUcAEhoQl.jpg)"></div>
+			<div style="background-image:url(http://i3.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large)"></div>
+			<div style="background-image:url(https://i.ytimg.com/vi/pDgM1ug0wVQ/maxresdefault.jpg)"></div>
+			<div style="background-image:url(http://p2.i.ntere.st/0fa87ebd7b66b5f7aa6baa24b6fdacb5_480.jpg)"></div>
+			<div style="background-image:url(https://i.gyazo.com/88d02d711b1be8733bcb52d5050c5457.png)"></div>
+			<div style="background-image:url(http://i1.kym-cdn.com/photos/images/facebook/001/037/049/75d.png)"></div>
+			<div style="background-image:url(https://static-cdn.jtvnw.net/emoticons/v1/28087/3.0)"></div>
+			<div style="background-image:url(http://cdn.i.ntere.st/p/19449802/image)"></div>
+			<div style="background-image:url(https://static-cdn.jtvnw.net/emoticons/v1/4339/3.0)"></div>
+			<div style="background-image:url(https://static-cdn.jtvnw.net/emoticons/v1/55338/3.0)"></div>
+			<div style="background-image:url(https://static-cdn.jtvnw.net/emoticons/v1/93064/3.0)"></div>
+			<div style="background-image:url(http://static.tumblr.com/b6e1cf0f096282785050ff2f77e49d6a/smihjlx/FHgmwwb2u/tumblr_static_kappa.gif)"></div>
 		</div>
 	</div>
 </div>
