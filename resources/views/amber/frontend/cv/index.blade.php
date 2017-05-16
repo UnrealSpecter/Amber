5549d43b03d0905fc5b9bf688d4b62098d71db59
vagrant <vagrant@homestead>
2017-05-16 15:17:35 +0000

    curriculum edit

--- resources/views/amber/frontend/cv/index.blade.php
+++ resources/views/amber/frontend/cv/index.blade.php
@@ -7,24 +7,24 @@
 @extends('layout')
 @section('content')
 @include('amber.resources.frontend-menu')
-	<div class="col-lg-10 col-lg-offset-2 col-md-11 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0" style="display: flex; align-items: center; flex-flow: column wrap;">
+	<div class="cv col-lg-10 col-lg-offset-2 col-md-11 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0">
 		@foreach ($categories as $category)
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden" style="margin-top: 50px;">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					@if ($category->id % 2 == 0)
-					<h1 class="animated fadeInRightBig text-left" style="font-weight: bold; color:#77aaff;">{{$category->categoryName}}</h1>
+					<h1 class="animated fadeInRightBig text-left category-name">{{$category->categoryName}}</h1>
 					@else
-					<h1 class="animated fadeInRightBig text-left" style="font-weight: bold; color:#77aaff;">{{$category->categoryName}}</h1>
+					<h1 class="animated fadeInRightBig text-left category-name">{{$category->categoryName}}</h1>
 					@endif
 				</div>
 			</div>
 			@foreach($category->entries as $entry)
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden">
 					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-4">
-						<h3 class="animated fadeInLeft text-left" style="color: white;">{{$entry->leftSide}}</h3>
+						<h3 class="animated fadeInLeft text-left entry-left">{{$entry->leftSide}}</h3>
 					</div>
 					<div class="col-lg-9 col-md-9 col-sm-9 col-xs-8">
-						<h3 class="animated fadeInRight text-left" style="color: white; font-style: italic;">{{$entry->rightSide}}</h3>
+						<h3 class="animated fadeInRight text-left entry-right" >{{$entry->rightSide}}</h3>
 					</div>
 				</div>
 			@endforeach
--- storage/framework/views/044e6fda74c314d5ca6774512e4a8e75742c0a54.php
+++ storage/framework/views/044e6fda74c314d5ca6774512e4a8e75742c0a54.php
@@ -7,24 +7,24 @@
 
 <?php $__env->startSection('content'); ?>
 <?php echo $__env->make('amber.resources.frontend-menu', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	<div class="col-lg-10 col-lg-offset-2 col-md-11 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0" style="display: flex; align-items: center; flex-flow: column wrap;">
+	<div class="cv col-lg-10 col-lg-offset-2 col-md-11 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0">
 		<?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden" style="margin-top: 50px;">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<?php if($category->id % 2 == 0): ?>
-					<h1 class="animated fadeInRightBig text-left" style="font-weight: bold; color:#77aaff;"><?php echo e($category->categoryName); ?></h1>
+					<h1 class="animated fadeInRightBig text-left category-name"><?php echo e($category->categoryName); ?></h1>
 					<?php else: ?>
-					<h1 class="animated fadeInRightBig text-left" style="font-weight: bold; color:#77aaff;"><?php echo e($category->categoryName); ?></h1>
+					<h1 class="animated fadeInRightBig text-left category-name"><?php echo e($category->categoryName); ?></h1>
 					<?php endif; ?>
 				</div>
 			</div>
 			<?php $__currentLoopData = $category->entries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $entry): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 category hidden">
 					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-4">
-						<h3 class="animated fadeInLeft text-left" style="color: white;"><?php echo e($entry->leftSide); ?></h3>
+						<h3 class="animated fadeInLeft text-left entry-left"><?php echo e($entry->leftSide); ?></h3>
 					</div>
 					<div class="col-lg-9 col-md-9 col-sm-9 col-xs-8">
-						<h3 class="animated fadeInRight text-left" style="color: white; font-style: italic;"><?php echo e($entry->rightSide); ?></h3>
+						<h3 class="animated fadeInRight text-left entry-right" ><?php echo e($entry->rightSide); ?></h3>
 					</div>
 				</div>
 			<?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
